98d0d305693d636c7947b05b332da9566ee428c7
Roman Arenas <roman.ea3232@gmail.com>
2023-12-28 23:58:18 -0300

    portfolio-2023

--- src/utils/MenuHamburguesa.jsx
+++ src/utils/MenuHamburguesa.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export const MenuHamburguesa = () => {
-    const [menuAbierto, setMenuAbierto] = useState(false);
+  const [menuAbierto, setMenuAbierto] = useState(false);
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center w-40">
       <button
-        className="text-3xl focus:outline-none"
+        className="text-5xl focus:outline-none" // Aumenta el tamaño del texto
         onClick={() => setMenuAbierto(!menuAbierto)}
       >
         &#8801; {/* Icono de hamburguesa */}
@@ -14,11 +14,11 @@ export const MenuHamburguesa = () => {
 
       {menuAbierto && (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex items-center justify-center">
-          <div className="bg-white p-4 rounded shadow-md">
-            <p className="text-2xl mb-4">Menú</p>
+          <div className="bg-white p-6 rounded shadow-md"> {/* Aumenta el padding */}
+            <p className="text-3xl mb-4">Menú</p> {/* Aumenta el tamaño del texto */}
             {/* Opciones de menú aquí */}
             <button
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+              className="bg-blue-500 text-white py-3 px-6 rounded hover:bg-blue-600" // Aumenta el tamaño del botón
               onClick={() => setMenuAbierto(false)}
             >
               Cerrar
